2251bb4a1756362a55ad450cb9dd628df9b49afb
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-06-01 10:14:47 -0700

    publishing

--- scripts/travis-decode.sh
+++ scripts/travis-decode.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-[ -z "${$ENCRYPTION_PASSWORD}" ] && exit 0;
+[ -z "$ENCRYPTION_PASSWORD}" ] && exit 0;
 
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out local.secring.gpg -d
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out local.pubring.gpg -d
